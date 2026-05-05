44534dfe60a698edbb2051f4046eb07d40862879
Richer Yang <fantasyworldidvtw@gmail.com>
2026-05-05 10:54:27 +0800

    修正 PHP 物件型態限制錯誤

--- includes/update.php
+++ includes/update.php
@@ -15,8 +15,8 @@ final class RY_WEZI_update
             return;
         }
 
-        if (version_compare($now_version, '2.1.3', '<')) {
-            RY_WEZI::update_option('version', '2.1.3', true);
+        if (version_compare($now_version, '2.1.4', '<')) {
+            RY_WEZI::update_option('version', '2.1.4', true);
         }
     }
 }
